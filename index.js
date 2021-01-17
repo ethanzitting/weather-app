f5fc05a402ffbec2c519a19813bd56419f8e68b5
Ethan Zitting <ethanzitting@gmail.com>
2021-01-17 07:37:23 -0700

    Added eslint and prettier to the environment. Replaced Jquery selectors with standard selectors, hid API key from git

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-$(document).ready(() => {
-  $('button').click(async () => {
-    // Get the value that the user has input
-    cityName = $('#location')
-    cityName = cityName.value;
-    console.log(cityName)
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  // Get the value that the user has input
+  let cityName = document.querySelector("#location").value;
+  console.log(cityName);
 
-    // Punch it into the API request
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=727bdceb982e48d1a67cdf8fa8f2c204`)
+  // Punch it into the API request
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${config.weatherAPIKey}`
+  )
     .then((response) => {
       // Process the returned JSON object
       console.log(response);
     })
     .catch((error) => {
       console.log(error);
-    })
-  })
-}) 
-
+    });
+});
